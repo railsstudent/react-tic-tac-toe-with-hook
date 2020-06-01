f5876e3171ca539431affc3e634f91fb7dd44ca3
railsstudent <cleung334@gmail.com>
2020-06-01 19:23:31 +0800

    Style square component

--- components/Board.js
+++ components/Board.js
@@ -1,24 +1,6 @@
 import React from 'react';
 import Square from './Square';
 
-// const style = {
-//   border: '2px solid darkblue',
-//   borderRadius: '10px',
-//   width: '250px',
-//   height: '250px',
-//   margin: '0 auto',
-//   display: 'grid',
-//   gridTemplate: 'repeat(3, 1fr) / repeat(3, 1fr)'
-// };
-
-// const Board = ({ squares, onClick }) => (
-//   <div style={style}>
-//     {squares.map((square, i) => (
-//       <Square key={i} value={square} onClick={() => onClick(i)} />
-//     ))}
-//   </div>
-// );
-
 const Board = ({ squares, onClick }) => (
   <div>
     <Square value="1" onClick={() => onClick()} />
